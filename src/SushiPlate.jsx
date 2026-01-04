cab19f0420e9b2ae3c8bd7baa7d904321f35fdce
a2cogiya <a2cogiya@gmail.com>
2026-01-04 21:43:58 +0900

    MUIコンポーネントを使用。 お皿種別の削除時に確認ダイアログを表示。

--- src/SushiPlate.jsx
+++ src/SushiPlate.jsx
@@ -1,4 +1,10 @@
 import { useState } from 'react';
+import { Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+
+import IconButton from '@mui/material/IconButton';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import ClearIcon from '@mui/icons-material/Clear';
 import './SushiPlate.css';
 
 export default function SushiPlate() {
@@ -6,13 +12,16 @@ export default function SushiPlate() {
   const [plateVal, setPlateVal] = useState('');
   const [plateNum, setPlateNum] = useState(0);
   const [plates, setPlates] = useState([]);
-  // const [sum, setSum] = useState(0);
+  const [open, setOpen] = useState(false);
 
   const handleChangePlateVal = e => {
     setPlateVal(e.target.value);
   };
 
-  const handlePlateTypeAdd = () => {
+  const handleAddPlateType = () => {
+    if (plateVal === '')
+      return;
+
     const newPlates = [
       ...plates,
       {
@@ -29,62 +38,101 @@ export default function SushiPlate() {
     setMaxId(id => id + 1);
   };
 
-  const handlePlateAdd = e => {
+  const handleAddPlate = e => {
     setPlates(plates.map(item => {
-        if (item.id === Number(e.target.dataset.id)) {
-            return {
-                ...item,
-                plateNum: item.plateNum + 1
-            };
-        } else {
-            return item;
-        }
+      if (item.id === Number(e.currentTarget.dataset.id)) {
+        return {
+          ...item,
+          plateNum: item.plateNum + 1
+        };
+      } else {
+        return item;
+      }
     }));
   };
 
-  const handlePlateDel = e => {
+  const handleDelPlate = e => {
     setPlates(plates.map(item => {
-        if (item.id === Number(e.target.dataset.id)) {
-            return {
-                ...item,
-                plateNum: item.plateNum >= 1 ? item.plateNum - 1 : 0
-            };
-        } else {
-            return item;
-        }
+      if (item.id === Number(e.currentTarget.dataset.id)) {
+        return {
+          ...item,
+          plateNum: item.plateNum >= 1 ? item.plateNum - 1 : 0
+        };
+      } else {
+        return item;
+      }
     }));
   };
 
-  const handlePlateTypeDel = e => {
+  const handleDelPlateType = e => {
     setPlates(plates.filter(item =>
-      item.id !== Number(e.target.dataset.id)
+      item.id !== Number(e.currentTarget.dataset.id)
     ));
+    setOpen(false);
   };
+
+  const handleOpenDelPlateTypeDialog = () => {
+    setOpen(true);
+
+  }
+
+  const handleCloseDelPlateTypeDialog = () => {
+    setOpen(false);
+  }
 
   return (
     <div>
-      <label>
-        <input type="text" name="title"
-          value={plateVal} onChange={handleChangePlateVal} />
-        円のお皿を
-      </label>
-      <button type="button"
-        onClick={handlePlateTypeAdd}>追加</button>
+      <TextField
+        required
+        label="お皿の値段"
+        defaultValue=""
+        variant="standard"
+        onChange={handleChangePlateVal}
+      />
+      円のお皿を
+      <Button variant="contained" startIcon={<AddCircleIcon />}
+        onClick={handleAddPlateType}
+      >
+        追加
+      </Button>
       <hr />
       <ul>
         {plates.map(item => (
           <li key={item.id}>
             {item.plateVal}円皿が
             {item.plateNum}枚
-            <button type="button"
-              onClick={handlePlateAdd} data-id={item.id}>+
-            </button>
-            <button type="button"
-              onClick={handlePlateDel} data-id={item.id}>-
-            </button>
-            <button type="button"
-              onClick={handlePlateTypeDel} data-id={item.id}>削除
-            </button>
+            <IconButton
+              color="primary"
+              onClick={handleAddPlate}
+              data-id={item.id}
+            >
+              <AddCircleIcon />
+            </IconButton>
+            <IconButton
+              color="primary"
+              onClick={handleDelPlate}
+              data-id={item.id}
+            >
+              <RemoveCircleIcon />
+            </IconButton>
+            <IconButton
+              color="warning"
+              onClick={handleOpenDelPlateTypeDialog}
+            >
+              <ClearIcon />
+            </IconButton>
+            <Dialog open={open} onClose={handleOpenDelPlateTypeDialog}>
+              <DialogTitle>削除の確認</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  本当にこのお皿全体を削除してもよろしいですか？この操作は取り消せません。
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleCloseDelPlateTypeDialog} autoFocus>キャンセル</Button>
+                <Button onClick={handleDelPlateType} color="error" data-id={item.id}>削除</Button>
+              </DialogActions>
+            </Dialog>
           </li>
         ))}
       </ul>
